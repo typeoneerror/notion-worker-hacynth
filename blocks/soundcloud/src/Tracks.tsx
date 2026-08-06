87dbe0f31ad84987b4a3313a0925ac00880a8c06
Benjamin Borowski <ben@weareokidoki.com>
2026-08-05 19:05:05 -0700

    Fixes missed typing issues

--- blocks/soundcloud/src/Tracks.tsx
+++ blocks/soundcloud/src/Tracks.tsx
@@ -29,7 +29,7 @@ export function Tracks({ currentTrack, tracks, error }: TracksProps) {
   return (
     <ul>
       {sortedTracks.map((track) => {
-        const isSelected = track.id === currentTrack.id;
+        const isSelected = track.id === currentTrack?.id;
         const className = isSelected ? 'selected' : undefined;
         const title = String(track.propertiesByKey.title ?? 'Untitled');
         return (
